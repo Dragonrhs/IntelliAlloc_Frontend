ec05a5e952d9c8937ced5cf7ddbe8cc34f2e22b9
Dragonrhs <Ryuske2009@hotmail.com>
2025-06-24 14:52:45 -0300

    <feat>[routes]: Implementado rotas de classificacao

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -24,9 +24,11 @@ import ViewAssetClassPage from './pages/ViewAssetClassPage';
 import ParametrosRebalanceamento from './pages/ParametrosRebalanceamento';
 import AvaliacaoMensalClasses from './pages/AvaliacaoMensalClasses';
 import PermissionsManagement from './pages/PermissionsManagement';
+import ClassificarAtivos from './pages/ClassificarAtivos';
 import { useUser } from './context/UserContext';
 import AccessDenied from './components/AccessDenied';
 import PageTransition from './components/PageTransition';
+import HistoricoClassificacao from './pages/HistoricoClassificacao';
 
 interface ProtectedRouteProps {
   children: React.ReactNode;
@@ -92,146 +94,160 @@ const AppRoutes: React.FC = () => {
 
   return (
     <PageTransition>
-      <Routes>
-        {/* Rota para a página inicial (redireciona para /login se não autenticado) */}
-        <Route path="/" element={
-          userRole ? <Navigate to="/home" replace /> : <Navigate to="/login" replace />
-        } />
+    <Routes>
+      {/* Rota para a página inicial (redireciona para /login se não autenticado) */}
+      <Route path="/" element={
+        userRole ? <Navigate to="/home" replace /> : <Navigate to="/login" replace />
+      } />
 
-        {/* Rota protegida para home */}
-        <Route path="/home" element={<PrivateRoute><Home /></PrivateRoute>} />
+      {/* Rota protegida para home */}
+      <Route path="/home" element={<PrivateRoute><Home /></PrivateRoute>} />
 
-        {/* Rota para login */}
-        <Route path="/login" element={<Login />} />
+      {/* Rota para login */}
+      <Route path="/login" element={<Login />} />
 
-        {/* Rota para registro */}
-        <Route path="/register" element={<Register />} />
+      {/* Rota para registro */}
+      <Route path="/register" element={<Register />} />
 
-        {/* Rota para o formulário de suitability (adicionar/editar cliente) */}
-        <Route path="/suitability" element={<ProtectedRoute><Suitability /></ProtectedRoute>} />
-        <Route path="/suitability/:clientId" element={<ProtectedRoute><Suitability /></ProtectedRoute>} />
+      {/* Rota para o formulário de suitability (adicionar/editar cliente) */}
+      <Route path="/suitability" element={<ProtectedRoute><Suitability /></ProtectedRoute>} />
+      <Route path="/suitability/:clientId" element={<ProtectedRoute><Suitability /></ProtectedRoute>} />
 
-        {/* Rota para detalhes de um cliente específico */}
-        <Route path="/client/:clientId" element={<ProtectedRoute><ClientDetails /></ProtectedRoute>} />
+      {/* Rota para detalhes de um cliente específico */}
+      <Route path="/client/:clientId" element={<ProtectedRoute><ClientDetails /></ProtectedRoute>} />
 
-        {/* Rota para a lista de clientes */}
-        <Route path="/clients" element={<ProtectedRoute><ClientsList /></ProtectedRoute>} />
+      {/* Rota para a lista de clientes */}
+      <Route path="/clients" element={<ProtectedRoute><ClientsList /></ProtectedRoute>} />
 
-        {/* Rota para o histórico */}
-        <Route path="/history" element={<ProtectedRoute><History /></ProtectedRoute>} />
+      {/* Rota para o histórico */}
+      <Route path="/history" element={<ProtectedRoute><History /></ProtectedRoute>} />
 
-        {/*Rota para o histórico do sistema */}
-        <Route path="/system-history" element={<AdminRoute><SystemHistory /></AdminRoute>} />
+      {/*Rota para o histórico do sistema */}
+      <Route path="/system-history" element={<AdminRoute><SystemHistory /></AdminRoute>} />
 
-        {/* Rota para gerenciamento de usuários */}
-        <Route path="/management" element={<AdminRoute><Management /></AdminRoute>} />
+      {/* Rota para gerenciamento de usuários */}
+      <Route path="/management" element={<AdminRoute><Management /></AdminRoute>} />
 
-        {/* Rota para recomendação de portfólio */}
-        <Route path="/recommended-portfolio" element={
+      {/* Rota para recomendação de portfólio */}
+      <Route path="/recommended-portfolio" element={
+        <ProtectedRoute>
+          <RecommendedPortfolio />
+        </ProtectedRoute>
+      } />
+
+      {/* Rota para visualização de recomendação de portfólio */}
+      <Route path="/view-recommended-portfolio" element={
+        <ProtectedRoute>
+          <ViewRecommendedPortfolio />
+        </ProtectedRoute>
+      } />
+
+      {/* Rota para estatísticas */}
+      <Route path="/estatisticas" element={
+        <ProtectedRoute>
+          <Estatisticas />
+        </ProtectedRoute>
+      } />
+
+      {/* Rota para inserir ativo */}
+      <Route path="/inserir-ativo" element={
+        <ProtectedRoute>
+          <InserirAtivo />
+        </ProtectedRoute>
+      } />
+
+      {/* Rota para atualizar ativo */}
+      <Route path="/atualizar-ativo" element={
+        <ProtectedRoute>
+          <AtualizarAtivo />
+        </ProtectedRoute>
+      } />
+
+      {/* Rota para consultar ativos */}
+      <Route path="/consultar-ativos" element={
+        <ProtectedRoute>
+          <ConsultarAtivos />
+        </ProtectedRoute>
+      } />
+
+        {/* Rota para classificar ativos */}
+        <Route path="/classificar-ativos" element={
           <ProtectedRoute>
-            <RecommendedPortfolio />
+            <ClassificarAtivos />
           </ProtectedRoute>
         } />
 
-        {/* Rota para visualização de recomendação de portfólio */}
-        <Route path="/view-recommended-portfolio" element={
+      {/* Rota para importar ativos em lote */}
+      <Route path="/importar-ativos-lote" element={
+        <ProtectedRoute>
+          <ImportarAtivosLote />
+        </ProtectedRoute>
+      } />
+
+      {/* Rota para escolha de inserção de ativo */}
+      <Route path="/escolha-inserir-ativo" element={
+        <ProtectedRoute>
+          <EscolhaInserirAtivo />
+        </ProtectedRoute>
+      } />
+
+      {/* Rotas para histórico de ativos */}
+      <Route path="/historico-ativo/:id" element={
+        <ProtectedRoute>
+          <HistoricoAtivo />
+        </ProtectedRoute>
+      } />
+      <Route path="/historico-ativo" element={
+        <ProtectedRoute>
+          <HistoricoAtivosList />
+        </ProtectedRoute>
+      } />
+
+      {/* Rota para avaliação de classes de ativos */}
+      <Route path="/asset-class-evaluation" element={
+        <ProtectedRoute>
+          <AssetClassEvaluation />
+        </ProtectedRoute>
+      } />
+
+      {/* Rota para visualização de avaliações de classes de ativos */}
+      <Route path="/view-asset-class" element={
+        <ProtectedRoute>
+          <ViewAssetClassPage />
+        </ProtectedRoute>
+      } />
+
+      {/* Rota para avaliação mensal de classes de ativos */}
+      <Route path="/avaliacao-mensal-classes" element={
+        <ProtectedRoute>
+          <AvaliacaoMensalClasses />
+        </ProtectedRoute>
+      } />
+
+      {/* Rota para parâmetros de rebalanceamento */}
+      <Route path="/parametros-rebalanceamento" element={
+        <ProtectedRoute>
+          <ParametrosRebalanceamento />
+        </ProtectedRoute>
+      } />
+
+      {/* Rota para gerenciamento de permissões */}
+      <Route path="/permissions" element={
+        <ProtectedRoute>
+          <PermissionsManagement />
+        </ProtectedRoute>
+      } />
+
+        {/* Rota para histórico de classificações */}
+        <Route path="/historico-classificacao" element={
           <ProtectedRoute>
-            <ViewRecommendedPortfolio />
+            <HistoricoClassificacao />
           </ProtectedRoute>
         } />
 
-        {/* Rota para estatísticas */}
-        <Route path="/estatisticas" element={
-          <ProtectedRoute>
-            <Estatisticas />
-          </ProtectedRoute>
-        } />
-
-        {/* Rota para inserir ativo */}
-        <Route path="/inserir-ativo" element={
-          <ProtectedRoute>
-            <InserirAtivo />
-          </ProtectedRoute>
-        } />
-
-        {/* Rota para atualizar ativo */}
-        <Route path="/atualizar-ativo" element={
-          <ProtectedRoute>
-            <AtualizarAtivo />
-          </ProtectedRoute>
-        } />
-
-        {/* Rota para consultar ativos */}
-        <Route path="/consultar-ativos" element={
-          <ProtectedRoute>
-            <ConsultarAtivos />
-          </ProtectedRoute>
-        } />
-
-        {/* Rota para importar ativos em lote */}
-        <Route path="/importar-ativos-lote" element={
-          <ProtectedRoute>
-            <ImportarAtivosLote />
-          </ProtectedRoute>
-        } />
-
-        {/* Rota para escolha de inserção de ativo */}
-        <Route path="/escolha-inserir-ativo" element={
-          <ProtectedRoute>
-            <EscolhaInserirAtivo />
-          </ProtectedRoute>
-        } />
-
-        {/* Rotas para histórico de ativos */}
-        <Route path="/historico-ativo/:id" element={
-          <ProtectedRoute>
-            <HistoricoAtivo />
-          </ProtectedRoute>
-        } />
-        <Route path="/historico-ativo" element={
-          <ProtectedRoute>
-            <HistoricoAtivosList />
-          </ProtectedRoute>
-        } />
-
-        {/* Rota para avaliação de classes de ativos */}
-        <Route path="/asset-class-evaluation" element={
-          <ProtectedRoute>
-            <AssetClassEvaluation />
-          </ProtectedRoute>
-        } />
-
-        {/* Rota para visualização de avaliações de classes de ativos */}
-        <Route path="/view-asset-class" element={
-          <ProtectedRoute>
-            <ViewAssetClassPage />
-          </ProtectedRoute>
-        } />
-
-        {/* Rota para avaliação mensal de classes de ativos */}
-        <Route path="/avaliacao-mensal-classes" element={
-          <ProtectedRoute>
-            <AvaliacaoMensalClasses />
-          </ProtectedRoute>
-        } />
-
-        {/* Rota para parâmetros de rebalanceamento */}
-        <Route path="/parametros-rebalanceamento" element={
-          <ProtectedRoute>
-            <ParametrosRebalanceamento />
-          </ProtectedRoute>
-        } />
-
-        {/* Rota para gerenciamento de permissões */}
-        <Route path="/permissions" element={
-          <ProtectedRoute>
-            <PermissionsManagement />
-          </ProtectedRoute>
-        } />
-
-        {/* Rota para páginas não encontradas (404) */}
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
+      {/* Rota para páginas não encontradas (404) */}
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
     </PageTransition>
   );
 };
