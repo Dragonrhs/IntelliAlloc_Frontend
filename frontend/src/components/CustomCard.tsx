4c073e07e302a18653ad25473cc288706bb279a9
Dragonrhs <Ryuske2009@hotmail.com>
2025-07-30 11:51:34 -0300

    <refactor>[components]: refatorado estilo do componente de card

--- frontend/src/components/CustomCard.tsx
+++ frontend/src/components/CustomCard.tsx
@@ -5,11 +5,13 @@ interface CustomCardProps {
   children: React.ReactNode;
   className?: string;
   isDarkMode?: boolean; // Adicionada prop para modo escuro/claro
+  title?: string;
 }
 
-const CustomCard: React.FC<CustomCardProps> = ({ children, className, isDarkMode }) => {
+const CustomCard: React.FC<CustomCardProps> = ({ children, className, isDarkMode, title }) => {
   return (
     <div className={`custom-card ${className || ''} ${isDarkMode ? 'dark-mode' : 'light-mode'}`}>
+      {title && <h2 className="custom-card-title">{title}</h2>}
       {children}
     </div>
   );
